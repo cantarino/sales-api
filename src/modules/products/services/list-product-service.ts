e4dd5f8aafb3c844e7887963a7ef894e8f73d9eb
Christian Cantarino <christian.cantarino@gmail.com>
2021-10-11 22:05:55 -0300

    add function type

--- src/modules/products/services/list-product-service.ts
+++ src/modules/products/services/list-product-service.ts
@@ -1,8 +1,9 @@
 import { getCustomRepository } from "typeorm";
+import { Product } from "../typeorm/entitites/product";
 import { ProductRepository } from "../typeorm/repositories/products-repository";
 
 export class ListProductService {
-  public async execute() {
+  public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
     const products = productsRepository.find();
