1e67f5954361af7f252eaf0442846619e35b0f21
Christian Cantarino <christian.cantarino@gmail.com>
2021-10-11 22:56:37 -0300

    set server to main functionc

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,28 +3,40 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import AppError from "../errors/app-error";
 import routes from "./routes";
-const app = express();
 
-app.use(cors());
-app.use(express.json());
+const main = async () => {
+  const app = express();
 
-app.use(routes);
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
+  app.use(cors());
+  app.use(express.json());
+
+  app.use(routes);
+  app.use(
+    (
+      error: Error,
+      request: Request,
+      response: Response,
+      next: NextFunction
+    ) => {
+      if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+          status: "error",
+          message: error.message,
+        });
+      }
+
+      return response.status(500).json({
         status: "error",
-        message: error.message,
+        message: "Internal server error",
       });
     }
+  );
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}!`);
+  });
+};
 
-    return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
-    });
-  }
-);
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}!`);
+main().catch((err) => {
+  console.log("error: ", err);
 });
