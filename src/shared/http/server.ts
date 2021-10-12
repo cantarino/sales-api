34e828e6ed96b6b212bf9d0c7ec28e9a455c7988
Christian Cantarino <christian.cantarino@gmail.com>
2021-10-12 13:42:06 -0300

    create setup file

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,32 +1,6 @@
-import "@shared/typeorm";
-import { errors } from "celebrate";
-import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import AppError from "../errors/app-error";
-import routes from "./routes";
+import app from "./app";
 
 const main = async () => {
-  const app = express();
-
-  app.use(cors());
-  app.use(express.json());
-
-  app.use(routes);
-  app.use(errors());
-  app.use((error: Error, _: Request, response: Response, __: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: "error",
-        message: error.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
-    });
-  });
   const port = process.env.PORT || 8080;
   app.listen(port, () => {
     console.log(`Server started on port ${port}!`);
