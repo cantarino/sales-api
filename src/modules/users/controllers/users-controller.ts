4a22788747d8449ce1ceb4df1add784aadfce7a0
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-18 14:43:37 -0300

    create user controller

---
+++ src/modules/users/controllers/users-controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../services/create-user-service";
+import { ListUserService } from "../services/list-user-service";
+
+export class ProductsController {
+  public async index(_: Request, res: Response): Promise<Response> {
+    const listUsers = new ListUserService();
+    const users = await listUsers.execute();
+
+    return res.json(users);
+  }
+
+  public async show(_: Request, res: Response): Promise<Response> {
+    return res.json({});
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+
+  public async update(_: Request, response: Response): Promise<Response> {
+    return response.json({});
+  }
+
+  public async delete(_: Request, response: Response): Promise<Response> {
+    return response.json({});
+  }
+}
