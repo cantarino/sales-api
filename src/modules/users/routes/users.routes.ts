c96e49894b53f875665b0ab17216060e3702ec8d
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-18 18:59:46 -0300

    add avatar service to controller

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,15 +1,22 @@
+import uploadConfig from "@config/upload";
 import { celebrate, Joi } from "celebrate";
 import { Router } from "express";
+import multer from "multer";
 import isAuth from "../../../shared/http/middlewares/is-auth";
+import { UserAvatarController } from "../controllers/user-avatar-controller";
 import { UsersController } from "../controllers/users-controller";
 
 const usersRouter = Router();
+
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
 const bodyValidator = {
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
+const upload = multer(uploadConfig);
 
 usersRouter.get("/", isAuth, usersController.index);
 
@@ -17,6 +24,13 @@ usersRouter.post(
   "/",
   celebrate({ body: bodyValidator }),
   usersController.create
+);
+
+usersRouter.patch(
+  "/avatar",
+  isAuth,
+  upload.single("avatar"),
+  usersAvatarController.update
 );
 
 export default usersRouter;
