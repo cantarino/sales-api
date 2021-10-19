cb5b5fb968eb8fa0a26cb06faf151a5e2428500d
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-19 16:42:36 -0300

    send forgot password email service

---
+++ src/modules/users/services/send-forgot-password-email-service.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/app-error";
+import { getCustomRepository } from "typeorm";
+import { UserTokensRepository } from "../typeorm/repositories/user-tokens-repository";
+import { UserRepository } from "../typeorm/repositories/users-repository";
+
+interface IRequest {
+  email: string;
+}
+
+export class SendOFrgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UserRepository);
+    const usersTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("There is no user with this email");
+    }
+
+    const userToken = usersTokenRepository.generate(user.id);
+
+    console.log(userToken);
+  }
+}
