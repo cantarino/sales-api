a486f30fe313227b97b91ba5f323d83070123d2d
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-19 17:25:14 -0300

    creating reset password controller

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { celebrate, Joi } from "celebrate";
 import { Router } from "express";
 import { ForgotPasswordController } from "../controllers/forgot-password-controller";
+import { ResetPasswordController } from "../controllers/reset-password-controller";
 
 const passwordsRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
+
 const bodyValidator = {
   email: Joi.string().email().required(),
 };
@@ -12,6 +15,18 @@ passwordsRouter.post(
   "/forgot",
   celebrate({ body: bodyValidator }),
   forgotPasswordController.create
+);
+
+passwordsRouter.post(
+  "/reset",
+  celebrate({
+    body: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref("password")),
+    },
+  }),
+  resetPasswordController.create
 );
 
 export default passwordsRouter;
