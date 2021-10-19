c56243d80edcc3f252b5e7167c1b3830bd03f078
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-19 19:51:28 -0300

    Update profile service

---
+++ src/modules/users/services/update-profile-service.ts
@@ -0,0 +1,51 @@
+import AppError from "@shared/errors/app-error";
+import { compare } from "bcryptjs";
+import { getCustomRepository } from "typeorm";
+import { User } from "../typeorm/entities/user";
+import { UserRepository } from "../typeorm/repositories/users-repository";
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  password?: string;
+  old_password?: string;
+}
+
+export class UpdateProfileService {
+  public async execute({
+    user_id,
+    email,
+    password,
+    old_password,
+    name,
+  }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UserRepository);
+
+    const user = await userRepository.findById(user_id);
+    if (!user) throw new AppError("User not found.");
+
+    const userWithEmail = await userRepository.findByEmail(email);
+    if (userWithEmail && userWithEmail.id == user_id)
+      throw new AppError("There is already a user with this mail");
+
+    if (password && !old_password)
+      throw new AppError("Old password is required");
+
+    if (password && old_password) {
+      const checkPassword = await compare(old_password, user.password);
+
+      if (!checkPassword) {
+        throw new AppError("Old password does not match");
+      }
+
+      user.password = password;
+    }
+
+    user.name = name;
+    user.email = email;
+    await userRepository.save(user);
+
+    return user;
+  }
+}
