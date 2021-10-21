f4b7764e0e1bbfd463fa3f62569d0abc01613248
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-21 16:04:10 -0300

    show order service

---
+++ src/modules/orders/services/show-order-service.ts
@@ -0,0 +1,22 @@
+import AppError from "@shared/errors/app-error";
+import { getCustomRepository } from "typeorm";
+import { Order } from "../typeorm/entities/order";
+import { OrdersRepository } from "../typeorm/repositories/orders-repository";
+
+interface IRequest {
+  id: string;
+}
+
+export class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+
+    const order = await ordersRepository.findById(id);
+
+    if (!order) {
+      throw new AppError("Order not found.");
+    }
+
+    return order;
+  }
+}
