f26fdca82c99832da97758e2af72f3e8564be0bd
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-22 00:12:08 -0300

    small refactor

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,7 @@
 export default {
   mail: {
     host: process.env.MAIL_HOST,
-    port: parseInt(process.env.MAIL_PORT ?? "2525"),
+    port: Number(process.env.MAIL_PORT),
     auth: {
       user: process.env.MAIL_AUTH_USER,
       pass: process.env.MAIL_AUTH_PASS,
