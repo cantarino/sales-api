582b1638522f2ce96a4cf959840c9d5adc8995c2
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-23 14:36:46 -0300

    users repository interface

---
+++ src/modules/users/domain/repositories/IUsersRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateUser } from "../entities/ICreateUser";
+import { IUser } from "../entities/IUser";
+
+export interface IUsersRepository {
+  findAll(): Promise<IUser[]>;
+  findByName(name: string): Promise<IUser | undefined>;
+  findById(id: string): Promise<IUser | undefined>;
+  findByEmail(email: string): Promise<IUser | undefined>;
+  create(data: ICreateUser): Promise<IUser>;
+  save(user: IUser): Promise<IUser>;
+  remove(user: IUser): Promise<void>;
+}
