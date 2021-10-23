f4b7376255031550a9301f9bd3dcb7599d87abdf
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-23 10:56:34 -0300

    implementing interface

--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,10 +1,22 @@
-import { EntityRepository, Repository } from "typeorm";
+import { EntityRepository, getRepository, Repository } from "typeorm";
+import { ICreateCustomer } from "../../../domain/models/ICreateCustomer";
+import { ICustomersRepository } from "../../../domain/repositories/ICustomerRepository";
 import { Customer } from "../entities/Customer";
 
 @EntityRepository(Customer)
-export class CustomersRepository extends Repository<Customer> {
+export class CustomersRepository implements ICustomersRepository {
+  constructor(private ormRepository: Repository<Customer>) {
+    this.ormRepository = getRepository(Customer);
+  }
+
+  public async findAll(): Promise<Customer[]> {
+    const customers = await this.ormRepository.find();
+
+    return customers;
+  }
+
   public async findByName(name: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         name,
       },
@@ -14,7 +26,7 @@ export class CustomersRepository extends Repository<Customer> {
   }
 
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         id,
       },
@@ -24,12 +36,29 @@ export class CustomersRepository extends Repository<Customer> {
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         email,
       },
     });
 
     return customer;
+  }
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = this.ormRepository.create({ name, email });
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
+  public async remove(customer: Customer): Promise<void> {
+    await this.ormRepository.remove(customer);
   }
 }
