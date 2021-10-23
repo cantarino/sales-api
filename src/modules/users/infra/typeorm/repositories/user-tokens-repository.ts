3e914cd16c0f2c96bca3177da0e77ff0a0bb5f8b
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-23 14:56:51 -0300

    implementing user token repository interface

--- src/modules/users/infra/typeorm/repositories/user-tokens-repository.ts
+++ src/modules/users/infra/typeorm/repositories/user-tokens-repository.ts
@@ -1,20 +1,27 @@
-import { Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
+import { IUserTokensRepository } from "../../../domain/repositories/IUserTokensRepository";
 import { UserToken } from "../entities/UserToken";
 
-export class UserTokensRepository extends Repository<UserToken> {
+export class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
   public async findByToken(token: string): Promise<UserToken | undefined> {
-    const userToken = await this.findOne({ where: { token } });
+    const userToken = await this.ormRepository.findOne({ where: { token } });
     return userToken;
   }
 
   public async findById(id: string): Promise<UserToken | undefined> {
-    const userToken = await this.findOne({ where: { id } });
+    const userToken = await this.ormRepository.findOne({ where: { id } });
     return userToken;
   }
 
   public async generate(user_id: string): Promise<UserToken> {
-    const userToken = this.create({ user_id });
-    await this.save(userToken);
+    const userToken = this.ormRepository.create({ user_id });
+    await this.ormRepository.save(userToken);
     return userToken;
   }
 }
